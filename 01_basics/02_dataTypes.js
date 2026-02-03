c3460c4b12a2933cd5c1d9175327491db184aa99
Sajid Ahmed <85191237+sazahm@users.noreply.github.com>
2026-02-03 23:38:04 +0000

    practiced js data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3+3 ) // we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Saz")
+
+let name = "Sajid"
+let age = 27
+let isLoggedIn = false
+
+
+// number => 2 to the power of 53
+// bigint => used for very big numbers
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+
+console.log(typeof null);
+console.log(typeof undefined);
