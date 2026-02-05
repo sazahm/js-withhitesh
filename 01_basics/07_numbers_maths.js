86bf1ddc0dbd3856fb54ce6b81b9143274f5791a
Sajid Ahmed <85191237+sazahm@users.noreply.github.com>
2026-02-05 03:11:50 +0000

    learnt about num and math

---
+++ 01_basics/07_numbers_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+const someNumber = 123.89765
+// console.log(someNumber.toPrecision(4));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// **************** Maths *******************
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.7));
+// console.log(Math.floor(4.7));
+// console.log(Math.min(4, 8, 2, 12));
+// console.log(Math.max(4, 8, 2, 12));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
