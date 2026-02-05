82ab04922b6246eeb76674d4beb675848a2947e2
Sajid Ahmed <85191237+sazahm@users.noreply.github.com>
2026-02-05 01:26:45 +0000

    practiced string operations

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,33 @@
+const name = "Sajid"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Saz-ahm-com')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-5, 6)
+console.log(anotherString);
+
+
+const newStringOne = "    Sajid    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://sajid.com/sajid%20ahmed"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('saz'));
+
+console.log(gameName.split('-'));
