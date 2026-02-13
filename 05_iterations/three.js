1031c6fcbb9d47987b879520a36bb5ef30007bbb
Sajid Ahmed <85191237+sazahm@users.noreply.github.com>
2026-02-13 09:33:57 +0000

    for of loops and maps

---
+++ 05_iterations/three.js
@@ -0,0 +1,45 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+const greetings = "Hello world!"
+
+for (const greet of greetings) {
+    // console.log(greet);
+    
+}
+
+
+// Maps
+
+const map = new Map()
+map.set('IN', 'India')
+map.set('USA', 'United States of America')
+map.set('FR', 'France')
+map.set('IN', 'India')
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+    
+}
+
+const myObj = {
+    game1: 'NFS'
+    game2: 'GTA'
+}
+
+// for (const [key, value] of myObj) {
+//     console.log(key, ':-', value);
+    
+// }
+
