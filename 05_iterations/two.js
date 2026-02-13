e93cf2791ec8c20e7133b87e0b2a4bfc10b5c4db
Sajid Ahmed <85191237+sazahm@users.noreply.github.com>
2026-02-13 07:04:26 +0000

    do and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+let index = 0
+// while (index <= 10) {
+//     console.log(index);
+//     index = index + 2
+// }
+
+let myArray = ["Naruto","Sasuke","Kakashi"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(myArray[arr]);
+    arr++
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
