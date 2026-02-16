5eb7e1144b5cf2060d302c89f1b369c2b24ac25c
Sajid Ahmed <85191237+sazahm@users.noreply.github.com>
2026-02-16 12:00:08 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,31 @@
+const myObj = {
+    js: 'javascript',
+    cpp:'C++',
+    rb: 'ruby',
+    swift: "swift by apple"
+}
+
+for (const key in myObj) {
+    // console.log(`${key} shortcut is for ${myObj[key]}`);
+    
+}
+
+
+const programming = ["js","cpp","py","java"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+
+// const map = new Map()
+// map.set('IN', 'India')
+// map.set('USA', 'United States of America')
+// map.set('FR', 'France')
+// map.set('IN', 'India')
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
